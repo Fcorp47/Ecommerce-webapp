11903702e2ba2c9a7f58f0d760bdd9aa304b1a2f
Fcorp47 <109166373+Fcorp47@users.noreply.github.com>
2022-11-08 00:54:31 +0530

    Add files via upload

---
+++ DAL/storeDAL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace AUshop.DAL
+{
+    public class storeDAL
+    {
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["conau"].ConnectionString);
+
+
+
+        public DataSet GetProductDetails(int ProductID)
+        {
+            try
+            {
+                string selectq = "select* from tblgame where ProductID=@ProductID";
+                SqlCommand objcmd = new SqlCommand(selectq, cn);
+
+                objcmd.Parameters.AddWithValue("@ProductID", ProductID);
+
+
+                cn.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(objcmd);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+    }
+}
