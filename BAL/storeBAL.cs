6bc0e34a090a5e70c5e06f80645f6cacd56e1272
Fcorp47 <109166373+Fcorp47@users.noreply.github.com>
2022-11-08 00:48:24 +0530

    Add files via upload

---
+++ BAL/storeBAL.cs
@@ -0,0 +1,19 @@
+﻿using AUshop.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace AUshop.BAL
+{
+    public class storeBAL
+    {
+    
+             storeDAL objDal = new storeDAL();
+        public DataSet ProductDetails(int ProductID)
+        {
+            return objDal.GetProductDetails(ProductID);
+        }
+    }
+}
